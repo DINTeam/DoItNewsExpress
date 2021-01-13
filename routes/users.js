45c4adaff579beb610fdbd318decd70f77085871
LAPTOP-SM9I0TV8\윤주관 <btob6461@naver.com>
2021-01-14 01:01:08 +0900

    no message

--- routes/users.js
+++ routes/users.js
@@ -3,150 +3,40 @@ var app= express();
 var router = express.Router();
 var serveStatic = require('serve-static');      //특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 열어주는 역할
 var path = require('path');
+var mysql = require('mysql');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 var expressErrorHandler = require('express-error-handler');
-
 var bodyParser_post = require('body-parser');
 
 //use사용해서 미들웨어 등록하기
 app.use(bodyParser_post.urlencoded({extende : false}));
 app.use(bodyParser_post.json());
-app.use(serveStatic(path.join(__dirname, 'public')));
 
+var conn = mysql.createConnection({
+  port : 3000,
+  user : 'admin',
+  password : 'doitnews',
+  database : 'doitnews'
+});
+conn.connect();
 
-app.use(expressSession({
-  secret : 'doitnews',
-  resave : true,
-  saveUninitialized : true
-}));
+router.post("/sign_up", function(req,res,next){
+  var body = req.body;
+  var email = body.email;
+  var phone = body.phone;
+  var pw = body.password;
 
-var router = express.Router();
-router.route('/addUser').post(
-    function(req,res) {
-        var paramID = req.body.id || req.qeury.id;
-        var paramPW = req.body.password || req.query.password;
-        var paramEMAIL = req.body.email || req.query.email;
-        var paramPHONE = req.body.phone || req.query.phone;
-        cosole.log('id : ' + paramID + ', paramPW : ' + paramPW + ',paramEmail :' + paramEMAIL + ", paramPHONE : " + paramPHONE);
-
-        addUser(paramID, paramPW, paramEMAIL, paramPHONE, function (err, result) {
-                if (err) {
-                    console.log("Error");
-                    res.wrtieHead(200);
-                    res.end();
-                    return;
-                }
-                if (result) {
-                    console.dir(result);
-                    res.writeHead(200);
-                    res.end();
-                } else {
-                    console.log('DB 추가 에러');
-                    res.writeHead(200);
-                    res.end();
-                }
-            }
-        );
-    }
-);
-
-router.route('/login').post(
-    function (req,res) {
-        var paramID = req.body.id || req.query.id;
-        var paramPW = req.body.password || req.query.password;
-        console.log('parmaID : '+paramID + ', paramPW : ' + paramPW);
-
-        authUser(paramID, paramPW, function(err, rows){
-            if (err) {
-                console.log("Error");
-                res.wrtieHead(200);
-                res.end();
-                return;
-            }
-            if (rows) {
-                console.dir(rows);
-                res.writeHead(200);
-                res.end();
-            } else {
-                console.log('empty Error');
-                res.writeHead(200);
-                res.end();
-            }
-        });
-    }
-);
-
-app.use('/', router);
-var addUser = function(id,password,email,phone,callback){
-    console.log('addUser 호출');
-
-    pool.getConnection(
-        function(err,poolConn)
-        {
-            if(err)
-            {
-                if(poolConn){poolConn.release();}
-                callback(err,null);
-                return;
-            }
-            console.log('데이터베이스 연결 스레드 아이디' + poolConn.threadId);
-            var data = {id : id, password : password , email : email, phone: phone};
-            var exec = poolConn.query('insert into user set ?',data, function(err,result){
-                poolConn.release();
-                console.log('실행된 sql : ' + exec.sql);
-
-                if(err){
-                    console.log('sql 실행 시 에러 발생');
-                    callback(err,null);
-                    return;
-                }
-                callback(null,result);
-            });
-        }
-    );
-}
-
-var authUser = function(id,password,callback){
-    console.log('input id: ' + id + ', pw : ' + password);
-
-    pool.getConnection(function(err,poolConn){
-        if(err){
-            if(poolConn){
-                poolConn.release();
-            }
-            callback(err,null);
-            return;
-        }
-        console.log('데이터베이스 연결 스레드 아이디' + poolConn.threadId);
-
-        var tablename='user';
-        var columns= ['id','email','phone'];
-
-        var exec=poolConn.query("select ?? from ?? where id = ? and password = ?", [columns, tablename, id, password],
-            function(err,rows){
-            poolConn.release();
-            console.log('실행된 sql : ' + exec.sql);
-
-            if(err){
-                callback(err,null);
-                return;
-            }
-            if(rows.length > 0){
-                console.log('사용자 확인');
-                callback(null, rows);
-            }else{
-                console.log('사용자 확인 불가');
-                callback(null,null);
-            }
-            });
-    });
-};
+  var query = conn.query('insert into user (user_email, user_pw,user_phone ) values ("' + email + '","' + phone + '","' + pw + '")', function(err, rows) {
+    if(err) { throw err;}
+    console.log("Data inserted!");
+  })
+})
 
 /* GET users listing. */
 /**
  * @swagger
- * tags:
+ * tags: -"tag-for-user"
  *   name: User
  *   description: 사용자 정보 가져오기
  */
@@ -157,11 +47,20 @@ var authUser = function(id,password,callback){
  *     summary:
  *     tags:
  *     parameters:
- *       - in:
- *         name:
- *         type:
- *         enum:
- *         description: |
+ *       - in : "query"
+ *         name : "password"
+ *         type : "string"
+ *         description : "사용자 비밀번호 정보"
+ *
+ *       - in : "query"
+ *         name : "email"
+ *         type : "string"
+ *         description : "사용자 이메일 정보"
+ *
+ *       - in : "query"
+ *         name : "phone"
+ *         type : "string"
+ *         description : "사용자 전화번호 정보"
  *
  *     responses:
  *       200:
