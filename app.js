00fa1155c6c7e3cbc151ba54e6871308ba48794a
kye8524 <kye8314@gmail.com>
2021-01-15 14:55:27 +0900

    모듈 수정

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+const asyncify = require('express-asyncify');
+
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerOption = require('./swagger');
 const swaggerUi = require('swagger-ui-express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = asyncify(express());
 
-var app = express();
+let tokenMiddleWare = require('./utils/tokenAuth');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,13 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(tokenMiddleWare);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const swaggerSpec = swaggerJSDoc(swaggerOption);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
